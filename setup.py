c6872907d85725d723401d136fe4b7f3e67ffa5b
Jirka Bruijn <jirka.bruijn@gmail.com>
2020-03-24 00:26:36 +0100

    A very simple test for pipinstallables

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'pipinstallable',
+    version = '0.0.1',
+    description = 'A test package to test pipinstallables',
+    url = 'git@github.com:jirkabruijn/pipinstallable.git',
+    author = 'Jiri Bruijn',
+    author_email = 'no@address.com',
+    license = '',
+    packages=['pipinstallable'],
+    zip_safe=False
+)
